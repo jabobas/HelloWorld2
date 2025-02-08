b2967f103baa4c874c49730a480a4a161d7fa0bf
jabobas <abbas.jabor@maine.edu>
2025-02-07 20:46:10 -0500

    "here ye" added

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 class hello 
 {
     public static void main(String[] args) {
+        System.out.println("Here ye : important announcement");
         System.out.println("Hello, World!");
+        
     }
 }
