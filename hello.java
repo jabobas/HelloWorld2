462670a01c9fe1044267728617350857f766fc13
jabobas <abbas.jabor@maine.edu>
2025-02-14 17:24:49 -0500

    Closes Better information output #1

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 class hello 
 {
     public static void main(String[] args) {
@@ -9,5 +11,9 @@ class hello
         System.out.println("hi denise");
         System.out.println("hi ester");
         System.out.println("bye bye");
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = currentDate.format(formatter);
+        System.out.println("Current Date: " + formattedDate);
     }
 }
